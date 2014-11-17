c8d0664cd27edeff2f42ae03064f553aa62f359f
Greggory Rothmeier <greggroth@gmail.com>
2014-11-17 12:21:29 -0800

    Revert "Second pass at implementing a base list class"
    This reverts commit 6a6cb2fad3052337ca634f609fc8a53ae5a395f0.
    
    Conflicts:
    	lib/netsuite/support/generic_list.rb

--- lib/netsuite/support/generic_list.rb
+++ lib/netsuite/support/generic_list.rb
@@ -1,48 +1,28 @@
 module NetSuite
   module Support
     class GenericList
-      include Support::Fields
 
-      class << self
+      # This is very experimental: idea to replace many of the 
+      # generic list classes that are cluttering up the code base
 
-        def list_class(klass)
-          @list_class = klass
-        end
-
-        def list_key(key)
-          field key
-
-          define_method(:sublist_key) { key }
-
-          define_method("#{key}=") do |list|
-            list = [ list ] if !list.is_a?(Array)
-
-            @list = list.map do |item|
-              @@list_class.new(item)
-            end
-          end
-
-          define_method("#{key}") do
-            @list ||= []
-          end
-        end
-
-      end
-
-      field :replace_all
+      attr_accessor :list
 
       def initialize(attributes = {})
-        initialize_from_attributes_hash(attributes)
+
+        @list_key = attributes.keys.first
+
+        list = attributes[@list_key]
+        list = [ list ] if !list.is_a?(Array)
+
+        @list = list.map do |item|
+          NetSuite::Records::RecordRef.new(item)
+        end
+
       end
 
       def to_record
-        rec = { "#{record_namespace}:#{sublist_key}" => send(self.sublist_key).map(&:to_record) }
-
-        if !replace_all.nil?
-          rec["#{record_namespace}:replaceAll"] = replace_all
-        end
-
-        rec
+        # TODO need a way to infer record namespace
+        { "#{record_namespace}:item" => @list.map(&:to_record) }
       end
 
     end
