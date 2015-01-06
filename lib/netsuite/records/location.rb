be98c31cad8b6ce22322248e5d5262678bc676f4
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2015-01-06 14:48:41 -0500

    Add #subsidiary_list to locations

--- lib/netsuite/records/location.rb
+++ lib/netsuite/records/location.rb
@@ -12,6 +12,8 @@ module NetSuite
       fields :addr1, :addr2, :addr3, :addr_phone, :addr_text, :addressee, :attention, :city, :country, :include_children,
         :is_inactive, :make_inventory_available, :make_inventory_available_store, :name, :override, :state, :tran_prefix, :zip
 
+      field :subsidiary_list, RecordRefList
+
       record_refs :logo, :parent
 
       attr_reader :internal_id
